8484bc23c0b86c162a42111243ca9875228acb66
udaysk3 <burluudaysantoshkumar3@gmail.com>
2023-07-21 22:26:24 +0530

    INIT

--- ProfileBuilder/settings.py
+++ ProfileBuilder/settings.py
@@ -125,10 +125,9 @@ import os
 # STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 # MEDIA_URL = 'media/'
 # MEDIA_ROOT = BASE_DIR / 'media'
-
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static') ]
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'staticfiles') ]
+STATIC_ROOT =os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
