caa534cab0caf233da87b000b596fd4a3b5ea151
udaysk3 <burluudaysantoshkumar3@gmail.com>
2023-07-21 22:32:20 +0530

    Static changes

--- ProfileBuilder/settings.py
+++ ProfileBuilder/settings.py
@@ -125,7 +125,7 @@ import os
 # STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 # MEDIA_URL = 'media/'
 # MEDIA_ROOT = BASE_DIR / 'media'
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'staticfiles') ]
 STATIC_ROOT =os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
